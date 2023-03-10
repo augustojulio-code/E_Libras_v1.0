6b7a1481f0adc66f02d9256fcf5afb08bb229fc0
Julio <julio.mech@hotmail.com>
2023-03-10 18:55:48 -0300

    Removendo anotation NonNull

--- app/src/main/java/com/example/e_libas_v_0_01/MainActivity.java
+++ app/src/main/java/com/example/e_libas_v_0_01/MainActivity.java
@@ -2,14 +2,13 @@ package com.example.e_libas_v_0_01;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.*;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,7 +21,6 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -125,7 +123,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>()
                                    {
                                        @Override
-                                       public void onComplete(@NonNull Task<AuthResult> task)
+                                       public void onComplete(Task<AuthResult> task)
                                        {
                                            if(task.isSuccessful())
                                            {
@@ -143,7 +141,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                                        .addOnCompleteListener(new OnCompleteListener<Void>()
                                                        {
                                                            @Override
-                                                           public void onComplete(@NonNull Task<Void> task)
+                                                           public void onComplete(Task<Void> task)
                                                            {
                                                                if (task.isSuccessful())
                                                                {
@@ -159,7 +157,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                                                                    .getUid()).setValue(score).addOnCompleteListener(new OnCompleteListener<Void>()
                                                                    {
                                                                        @Override
-                                                                       public void onComplete(@NonNull Task<Void> task)
+                                                                       public void onComplete( Task<Void> task)
                                                                        {
                                                                            if (task.isSuccessful())
                                                                            {
